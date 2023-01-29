f82d361ee38dd7d9cd9e069b533d473e6cb32b60
Erasil <erasil.kazah222@gmail.com>
2023-01-29 15:57:09 +0600

    hello world

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(null=True, blank=True)
+
